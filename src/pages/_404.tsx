91a0c9a39b0309bd68ff6b1b4b45401228b9824e
Vlad Dumitru <vlad-dumitru@outlook.com>
2023-03-11 20:19:40 +0200

    Adăugare pagini pentru coduri de eroare http

---
+++ src/pages/_404.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+import ConverseaError from '~/client/components/converseaError';
+
+const Error404Page: FC = () => {
+  return <ConverseaError />;
+};
+
+export default Error404Page;
