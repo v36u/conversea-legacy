60789f6db544fa7504090da32d5f49105ffa099a
Vlad Dumitru <vlad-dumitru@outlook.com>
2023-04-18 23:23:00 +0300

    Home page wip

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,82 +1,135 @@
 import {
-  Anchor,
-  BackgroundImage,
-  Box,
   Button,
   Container,
-  Loader,
-  Stack,
+  createStyles,
+  Group,
+  Image,
+  List,
+  rem,
+  Text,
+  ThemeIcon,
   Title,
 } from '@mantine/core';
 import { GetServerSideProps } from 'next';
-import { signOut, useSession } from 'next-auth/react';
-import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 import { FC } from 'react';
+
+const useStyles = createStyles((theme) => ({
+  inner: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    paddingTop: `calc(${theme.spacing.xl} * 4)`,
+    paddingBottom: `calc(${theme.spacing.xl} * 4)`,
+  },
+
+  content: {
+    maxWidth: rem(480),
+    marginRight: `calc(${theme.spacing.xl} * 3)`,
+
+    [theme.fn.smallerThan('md')]: {
+      maxWidth: '100%',
+      marginRight: 0,
+    },
+  },
+
+  title: {
+    color: theme.colorScheme === 'dark' ? theme.white : theme.black,
+    fontFamily: `Greycliff CF, ${theme.fontFamily}`,
+    fontSize: rem(44),
+    lineHeight: 1.5,
+    fontWeight: 900,
+
+    [theme.fn.smallerThan('xs')]: {
+      fontSize: rem(28),
+    },
+  },
+
+  control: {
+    [theme.fn.smallerThan('xs')]: {
+      flex: 1,
+    },
+  },
+
+  image: {
+    flex: 1,
+
+    [theme.fn.smallerThan('md')]: {
+      display: 'none',
+    },
+  },
+
+  highlight: {
+    position: 'relative',
+    backgroundColor: theme.fn.variant({
+      variant: 'light',
+      color: theme.primaryColor,
+    }).background,
+    borderRadius: theme.radius.sm,
+    padding: `${rem(3)} ${rem(12)}`,
+  },
+}));
 
 const IndexPage: FC = () => {
   const { status } = useSession();
+  const { classes } = useStyles();
 
   return (
-    <Container mt="xl" fluid>
-      {status === 'loading' && <Loader variant="bars" color="cyan" />}
-      {status === 'unauthenticated' && (
-        <>
-          <Link href="/auth/login/">
-            <Anchor size="sm" component="button">
-              Autentifică-te
-            </Anchor>
-          </Link>
-          <br />
-          <Link href="/auth/register/">
-            <Anchor size="sm" component="button">
-              Înregistrează-te
-            </Anchor>
-          </Link>
-        </>
-      )}
-      {status === 'authenticated' && (
-        <Button onClick={async () => await signOut()}>Deautentificare</Button>
-      )}
+    <div>
+      <Container>
+        <div className={classes.inner}>
+          <div className={classes.content}>
+            <Title className={classes.title}>
+              Travel the world through language with{' '}
+              <span className={classes.highlight}>Conversea</span>!
+            </Title>
+            <Text color="dimmed" mt="md">
+              Build fully functional accessible web applications faster than
+              ever - Mantine includes more than 120 customizable components and
+              hooks to cover you in any situation
+            </Text>
 
-      <Title order={1} align="center">
-        Conversea
-      </Title>
-
-      <Title mt={50} order={3} align="center">
-        Insert pompous motto
-      </Title>
-
-      <Box
-        mx="auto"
-        mt={50}
-        p="md"
-        h={400}
-        w={400}
-        sx={(theme) => ({
-          backgroundColor: theme.colors.indigo[0],
-          textAlign: 'center',
-          padding: theme.spacing.xl,
-          borderRadius: theme.radius.md,
-        })}
-      >
-        <BackgroundImage src="/assets/logoConversea.png">
-          <Stack mx="auto" align="center" h={400} spacing="xl" justify="center">
-            <Button
-              variant="gradient"
-              gradient={{ from: '#ed6ea0', to: '#ec8c69', deg: 35 }}
+            <List
+              mt={30}
+              spacing="sm"
+              size="sm"
+              icon={
+                <ThemeIcon size={20} radius="xl">
+                  X
+                </ThemeIcon>
+              }
             >
-              Inregistrare
-            </Button>
-            <Button
-              variant="gradient"
-              gradient={{ from: 'indigo', to: 'cyan' }}
-            >
-              Logare
-            </Button>
-          </Stack>
-        </BackgroundImage>
-      </Box>
-    </Container>
+              <List.Item>
+                <b>TypeScript based</b> – build type safe applications, all
+                components and hooks export types
+              </List.Item>
+              <List.Item>
+                <b>Free and open source</b> – all packages have MIT license, you
+                can use Mantine in any project
+              </List.Item>
+              <List.Item>
+                <b>No annoying focus ring</b> – focus ring will appear only when
+                user navigates with keyboard
+              </List.Item>
+            </List>
+
+            <Group mt={30}>
+              <Button radius="xl" size="md" className={classes.control}>
+                Get started
+              </Button>
+              <Button
+                variant="default"
+                radius="xl"
+                size="md"
+                className={classes.control}
+              >
+                Source code
+              </Button>
+            </Group>
+          </div>
+          <Image src="/assets/logoConversea.png" className={classes.image} />
+        </div>
+      </Container>
+    </div>
   );
 };
 
